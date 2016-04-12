a8d14bf12a5887f4919bbdab621c6536792d1665
Caelestor <richiou@berkeley.edu>
2016-04-12 14:10:14 -0700

    All 4 functions written, time to debug

--- code/reconstruct_3d.py
+++ code/reconstruct_3d.py
@@ -172,6 +172,35 @@ def fundamental_matrix(matches):
         residual = residual + error
     return (final, residual)
     
+def find_rotation_translation():
+	E = np.matrix('-0.00310695,-0.0025646,2.96584;-0.028094,-0.00771621,56.3813;13.1905,-29.2007,-9999.79')
+	t = []
+	R = []
+	R_det = []
+	#+-90 degree rotation
+	RCW90_t = np.transpose(np.matrix('0,-1,0;1,0,0;0,0,1'))
+	RCCW90_t = np.transpose(np.matrix('0,1,0;-1,0,0;0,0,1'))
+
+	#SVD
+	U,Sig,V_t = np.linalg.svd(E)
+	U_t = np.transpose(U)
+
+	#t & R
+	sign = [1,-1]
+	rot = [RCW90_t,RCCW90_t]
+	for s in sign:
+		t.append(s*U[:,2])
+		for r in rot:
+			R_temp = s*np.transpose(np.dot(np.dot(U,r),V_t))
+			R.append(R_temp)
+			R_det.append(np.linalg.det(R_temp))
+				
+	for i in range(0,2):
+		ind = R_det.index(max(R_det))
+		R_det.pop(ind)
+		R.pop(ind)
+	return R, t
+    
 def find_3d_points( K1, K2, R, t, x1, x2 ):
 
     # K1, K2, are the (3 x 3) camera matrices for the two cameras C1, C2
